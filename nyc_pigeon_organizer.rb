5f228ba16579a06ef08753252ddbba594f143d0e
DJ Draper <darcyjdraper@gmail.com>
2020-07-24 19:37:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,13 @@
 def nyc_pigeon_organizer(data)
-  new_hash = { }
+  new_hash = {}
   data.each do |key, value|
     value.each do |new_value, names|
       names.each do |name|
         if !new_hash[name]
-          new_hash[name] = { }
+          new_hash[name] = {}
         end
         if !new_hash[name][key]
-          new_hash[name][key] = [ ]
+          new_hash[name][key] = []
         end
         new_hash[name][key] << new_value.to_s
       end
